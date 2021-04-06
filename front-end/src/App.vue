d5793e2bb0a866c7bc2b47edd75ad2276ce8ca4a
extremepayne <harrison.payne@gmail.com>
2021-04-05 18:00:56 -0600

    Add header and footer

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,10 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        <router-link class="navbar-brand" to="/">extremepayne</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <main role="main" class="container">
+      <router-view />
+    </main>
+    <footer class="footer">
+      <div class="container">
+        <a target="_blank" href="https://github.com/extremepayne/learn-vim-org">Code for this website</a>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -15,6 +36,15 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+nav.bg-primary, header {
+  background-color: #34495e !important;
+}
+
+nav {
+  margin-right: auto;
+  margin-left: auto;
 }
 
 #nav {
@@ -28,5 +58,41 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+/* Sticky footer styles
+-------------------------------------------------- */
+html {
+  position: relative;
+  min-height: 100%;
+}
+body {
+  /* Margin bottom by footer height */
+  margin-bottom: 60px;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 60px;
+  line-height: 60px; /* Vertically center the text there */
+  background-color: #f5f5f5;
+}
+
+@media only screen and (min-width: 768px){
+  .navbar {
+    max-width: 720px;
+  }
+}
+@media only screen and (min-width: 992px){
+  .navbar {
+    max-width: 960px;
+  }
+}
+@media only screen and (min-width: 1200px){
+  .navbar {
+    max-width: 1140px;
+  }
 }
 </style>
