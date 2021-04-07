a96f92ff44f132fe4d5ef0a5ae115378c5b1ae25
extremepayne <harrison.payne@gmail.com>
2021-04-06 20:35:57 -0600

    we are going into bad commit message mode

--- front-end/src/views/Admin.vue
+++ front-end/src/views/Admin.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Admin page</h1>
+    <h2>Add a theme</h2>
+    <div>
+      <div class="form">
+        <input v-model="name" placeholder="Name">
+        <p></p>
+        <input v-model="description" placeholder="Description">
+        <p></p>
+        <input v-model="color" placeholder="Hex of color">
+        <p></p>
+        <button @click="createTheme">Create</button>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'Admin',
+  data() {
+    return {
+      name: "",
+      description: "",
+      color: "",
+    }
+  },
+  methods: {
+    async createTheme() {
+      try {
+        await axios.post('api/items', {
+          name: this.name,
+          description: this.description,
+          color: this.color,
+        });
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+}
+</script>
